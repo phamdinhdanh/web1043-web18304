060f4abc6f672638cd022a30e451fb83f5ab46a5
phamdinhdanh <danhpdpd08107@fpt.edu.vn>
2023-03-10 17:48:30 +0700

    lab1

---
+++ lab1/lab1.js
@@ -0,0 +1,9 @@
+// nhap a tu ban phim
+let a = prompt("Nhap so nguyen a:");
+//let a1 = parseInt
+//nhap b tu ban phim
+let b = prompt("Nhap so nguyen b:")
+//let b1 = parseInt
+let c = parseInt(a)+parseInt(b);
+document.write(c);
+console.log(c);
